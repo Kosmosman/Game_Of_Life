62912e98403858f2b7e81ae6e74616967bcc50f5
Joaquina Daeron <joaquind@ev-p3.kzn.21-school.ru>
2022-08-27 20:44:16 +0300

    Разбито на большее количество функций

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -3,11 +3,12 @@
 #include <unistd.h>
 #define HIGHT 25
 #define WIDTH 80
-void check_alive(char start[][WIDTH], char finish[][WIDTH], int i, int j);
+int check_alive(char start[][WIDTH], char finish[][WIDTH], int i, int j);
 void copy_matrix(char start[][WIDTH], char finish[][WIDTH]);
-int printing(char start[][WIDTH], char finish[][WIDTH]);
-void first_printing(char start[][WIDTH], char finish[][WIDTH], int* coordinates);
+void printing(char finish[][WIDTH]);
+void first_move(char start[][WIDTH], char finish[][WIDTH], int* coordinates);
 void zero(char start[][WIDTH], char finish[][WIDTH]);
+int change_field(char start[][WIDTH], char finish[][WIDTH]);
 
 int main(void) {
     char start[HIGHT][WIDTH], finish[HIGHT][WIDTH], ch;
@@ -15,28 +16,37 @@ int main(void) {
     printf("Добро пожаловать в Game Of Life! "
            "Для выхода введите q, для начал игры - любую другую клавишу.\n");
     while ((ch = getchar()) != 'q') {
-        first_printing(start, finish, coordinates);
-        while (printing(start, finish) > 0) {
+        zero(start, finish);
+        first_move(start, finish, coordinates);
+        getchar();
+        while (change_field(start, finish) > 0) {
             getchar();
+            printing(finish);
             copy_matrix(start, finish);
         }
+        getchar();
+        printf("Вы закончили игру! Для начала нового хода введите любой символ, для выхода введите q.\n");
     }
     return 0;
 }
 // Проверка клетки на жизнеспособность
-void check_alive(char start[][WIDTH], char finish[][WIDTH], int row, int column) {
-    int count = 0;
+int check_alive(char start[][WIDTH], char finish[][WIDTH], int row, int column) {
+    int count = 0, flag = 0;
     for (int m = row - 1; m <= row + 1; m++)
         for (int n = column - 1; n <= column + 1; n++)
-            if ((start[m][n] == '@') && !(m == row && n == column)) {
+            if ((start[m][n] == '@') && !(m == row && n == column))
                 count++;
-            }
     if (count < 2 || count > 3)
         finish[row][column] = '.';
-    if (count == 3)
+    if (count == 3) {
         finish[row][column] = '@';
-    if (count == 2 && start[row][column] == '@')
+        flag = 1;
+    }
+    if (count == 2 && start[row][column] == '@') {
         finish[row][column] = '@';
+        flag = 1;
+    }
+    return flag;
 }
 // Копируем матрицу из финальной версии в стартовую на каждом новом ходу
 void copy_matrix(char start[][WIDTH], char finish[][WIDTH]) {
@@ -45,20 +55,14 @@ void copy_matrix(char start[][WIDTH], char finish[][WIDTH]) {
             start[i][j] = finish[i][j];
 }
 // Печатаем поле со всеми клетками
-int printing(char start[][WIDTH], char finish[][WIDTH]) {
-    int i, j, cells = 0;
-    for (i = 1; i < HIGHT - 1; i++) {
-        for (j = 1; j < WIDTH - 1; j++) {
-            check_alive(start, finish, i, j);
+void printing(char finish[][WIDTH]) {
+    for (int i = 1; i < HIGHT - 1; i++) {
+        for (int j = 1; j < WIDTH - 1; j++) {
             printf("%c", finish[i][j]);
-            if (finish[i][j] == '@')
-                cells++;
         }
         printf("\n");
     }
-    printf("cells = %d\n", cells);
     printf("\n\n");
-    return cells > 0;
 }
 // Заполняем поле пробелами перед стартом каждой игры
 void zero(char start[][WIDTH], char finish[][WIDTH]) {
@@ -68,21 +72,25 @@ void zero(char start[][WIDTH], char finish[][WIDTH]) {
             finish[i][j] = '.';
         }
 }
-// Печатает поле с введенными входными данными
-void first_printing(char start[][WIDTH], char finish[][WIDTH], int* coordinates) {
+// Вводим входные данные и печатаем новое поле
+void first_move(char start[][WIDTH], char finish[][WIDTH], int* coordinates) {
     int count = 0;
-    zero(start, finish);
     printf("Введите координаты начальных клеток в формате \"y x\"."
            "Для окончания ввода введите любой нечисловой символ.\n");
     while (scanf("%d%d", &coordinates[count], &coordinates[count + 1]) == 2)
         count += 2;
-    for (int y = 0, x = 1; y < count; x += 2, y += 2)
+    for (int y = 0, x = 1; y < count; x += 2, y += 2) {
         start[coordinates[y]][coordinates[x]] = '@';
-    for (int i = 0; i < HIGHT; i++) {
-        for (int j = 1; j < WIDTH; j++) {
-            printf("%c", start[i][j]);
-        }
-        printf("\n");
+        finish[coordinates[y]][coordinates[x]] = '@';
     }
-    printf("\n\n");
+    printing(finish);
+}
+// Изменение клеток, отображаемое на конечном поле finish
+int change_field(char start[][WIDTH], char finish[][WIDTH]) {
+    int flag = 0;
+    for (int i = 1; i < HIGHT - 1; i++)
+        for (int j = 1; j < WIDTH - 1; j++)
+            if (check_alive(start, finish, i, j))
+                flag = 1;
+    return flag;
 }
